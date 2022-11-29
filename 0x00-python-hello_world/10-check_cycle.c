d9d7e1fa0a58b4253531b227bc16f8db1c0f8e8d
TKMshengu <tokocha81@gmail.com>
2022-11-29 15:44:20 +0200

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,19 +1,31 @@
 #include "lists.h"
-#include <stdio.h>
+
+/**
+ * check_cycle - check for loop in LL
+ * @list: head of linked list
+ *
+ * Description - check for loops in LL
+ * Return: 1 if cycled, 0 if not
+ */
 
 int check_cycle(listint_t *list)
 {
-	listint_t *cursor;
-	listint_t *head;
+	listint_t *slow, *fast;
 
-	head = list;
-	cursor = list;
-	while (cursor != NULL && cursor->next != NULL)
+	if (!list)
 	{
-		head = head->next;
-		cursor = cursor->next->next;
-		if (head == cursor)
+		return (0);
+	}
+	slow = list;
+	fast = list->next;
+	while (fast && slow && fast->next)
+	{
+		if (slow == fast)
+		{
 			return (1);
+		}
+		slow = slow->next;
+		fast = fast->next->next;
 	}
 	return (0);
 }
