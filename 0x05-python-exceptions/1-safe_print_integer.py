9c96eef95825d5ddae21b12267f1fc1dbb61ebe0
TKMshengu <tokocha81@gmail.com>
2022-12-20 14:18:58 +0200

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
-#i/usr/bin/python3
+#!/usr/bin/python3
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return true
-    except:
-        return
+        return(True)
+    except(ValueError, TypeError):
+        return(False)
