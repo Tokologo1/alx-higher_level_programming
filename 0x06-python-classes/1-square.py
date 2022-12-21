0400da7d3397a3f999b170a18ef90659be1f5520
tokologo1 <tokocha81@gmail.com>
2022-12-21 05:23:41 -0800

    Python classess

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines the square class"""
+
+class SQuare:
+    """Square class. Has a size"""
+    def __init__(self, sezevar):
+        self.__size =sizevar
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Defines the square class"""
+
+class Square:
+    """Sqaure class. Has a size"""
+    def __init__(self, size=0):
+        """Initilize Square"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("Size must be >= 0")
+        self.__size = size
+
+        def area(self):
+            """Returns area of the square"""
+            return self.__size * self.__size
+
+        @property
+        def size(self):
+            """Return size of the square"""
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            """Set size of the square"""
+            if type(value) is not int:
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+
+            def my_print(self):
+                if self.__size == 0:
+                    print()
+                else:
+                    str = '#' * self.__size
+                    for x in range(self.__size):
+                        print(str)
