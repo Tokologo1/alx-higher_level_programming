26bee3f2e8bd61f001cc25feedbe251e1466df68
TKMshengu <tokocha81@gmail.com>
2023-01-18 08:40:40 +0200

    Delete 0x06-python-classes directory

--- 0x06-python-classes/1-square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Defines the square class"""
-
-class SQuare:
-    """Square class. Has a size"""
-    def __init__(self, sezevar):
-        self.__size =sizevar
--- 0x06-python-classes/2-square.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Definws class. has a size"""
-def __init__(self, size=0):
-    if type(size) is not int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise valueError("size must be >= 0")
-    self.__size = size
