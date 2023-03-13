8de247da61f5cc1a177a56af6ecb011d63c458b9
tokologo1 <tokocha81@gmail.com>
2023-03-13 11:28:19 -0700

    Js Node

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+//Script that prints "JavaScript is Amazing"
+
+const myVar = "JavaScript is amazing";
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+// Script that prints C is a fun,Python is coll and JavaScript is Amazing.
+
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myVar);
