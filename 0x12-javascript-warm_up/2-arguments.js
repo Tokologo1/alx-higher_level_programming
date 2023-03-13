2a0db75b37b27a362d709b08ce19d23c677d1c9b
TKMshengu <tokocha81@gmail.com>
2023-03-13 20:41:15 +0200

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
+
+//Add arguments in the parameters
+
 const count = process.argv.length;
 console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
