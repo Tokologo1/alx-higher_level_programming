6918bdca8002bda814a8677b7c94ec06a4eac25c
tokologo1 <tokocha81@gmail.com>
2023-03-13 12:36:51 -0700

    Removing Js Files

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-//Script that prints "JavaScript is Amazing"
-
-const myVar = "JavaScript is amazing";
-console.log(myVar);
--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-
-//Concat String
-
-console.log(`${process.argv[2]} is ${process.argv[3]}`);
