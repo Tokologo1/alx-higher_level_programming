c348d1bc6f370e8f82266cae73ebcc75b9943bab
TKMshengu <tokocha81@gmail.com>
2023-03-14 19:27:59 +0200

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,3 @@
-#!/usr/bin/node
-
-// Script that prints C is fun,Python is cool and JavaScript is amazing.
-
+#!/usr/bin/nodw
 const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
 console.log(myVar);
