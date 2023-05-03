2922888021f732b5d3ccf6792ab5f65ec9ef0dae
“Tokologo1” <tokocha81@gmail.com>
2023-05-03 04:54:11 -0700

    0x15-javascript-web_jquery

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,3 @@
+$('DIV#toggle_header').click(fuction () {
+	$('HEADER').toggleClass('green red');
+});
---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,3 @@
+$('DIV#ass_item').click(function () {
+	$('UL.my_list').append('<li>Item</li>');
+});
